c63e75b5a50206d862aa8097b322e7d4327dd743
Balamurugan <balamurugan.dha@gmail.com>
2024-02-08 22:19:40 +0530

    Update Test2.java

--- src/test/java/com/balamurugan/Test2.java
+++ src/test/java/com/balamurugan/Test2.java
@@ -50,52 +50,6 @@ Sub HideColumns()
     For Each col In ActiveSheet.UsedRange.Columns
         hideColumn = True ' Assume column will be hidden unless proven otherwise
         For Each cell In col.Cells
-            If cell.Interior.Color <> referenceColor Then
-                hideColumn = True ' At least one cell doesn't match, so hide column
-                Exit For
-            Else
-                hideColumn = False ' All cells match, so don't hide column
-            End If
-        Next cell
-        col.EntireColumn.Hidden = hideColumn
-    Next col
-End Sub
-    */
-
-/**
-Sub HideColumns()
-    Dim referenceColor As Long
-    referenceColor = Range("M17").Interior.Color
-
-    Dim col As Range
-    Dim cell As Range
-    Dim hideColumn As Boolean
-
-    For Each col In ActiveSheet.UsedRange.Columns
-        hideColumn = True ' Assume column will be hidden unless proven otherwise
-        For Each cell In col.Cells
-            If cell.Interior.Color = referenceColor Then
-                hideColumn = False ' At least one cell matches, so don't hide column
-                Exit For
-            End If
-        Next cell
-        If hideColumn Then col.EntireColumn.Hidden = True
-    Next col
-End Sub
-    */
-
-/**
-Sub HideColumns()
-    Dim referenceColor As Long
-    referenceColor = Range("M17").Interior.Color
-
-    Dim col As Range
-    Dim cell As Range
-    Dim hideColumn As Boolean
-
-    For Each col In ActiveSheet.UsedRange.Columns
-        hideColumn = True ' Assume column will be hidden unless proven otherwise
-        For Each cell In col.Cells
             If cell.Interior.Color = referenceColor Then
                 hideColumn = False ' At least one cell matches, so don't hide column
                 Exit For
@@ -103,7 +57,7 @@ Sub HideColumns()
         Next cell
         If hideColumn Then
             col.EntireColumn.Hidden = True
-            Debug.Print "Column " & col.Column & " hidden."
+            MsgBox "Column " & col.Column & " hidden."
         End If
     Next col
 End Sub
