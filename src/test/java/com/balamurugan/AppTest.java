ff04b7529c6ebc92f9b4b67613656ce0cabe2d61
Balamurugan <balamurugan.dha@gmail.com>
2024-02-08 21:22:51 +0530

    Update AppTest.java

--- src/test/java/com/balamurugan/AppTest.java
+++ src/test/java/com/balamurugan/AppTest.java
@@ -36,3 +36,31 @@ public class AppTest
         assertTrue( true );
     }
 }
+
+/**
+Sub HideColumnsBasedOnColor()
+    Dim rng As Range
+    Dim cell As Range
+    Dim lastRow As Long
+    Dim lastColumn As Long
+    Dim col As Long
+    
+    'Change Sheet1 to the name of your sheet
+    Set rng = ThisWorkbook.Sheets("Sheet1").UsedRange
+    
+    lastRow = rng.Rows.Count
+    lastColumn = rng.Columns.Count
+    
+    'Loop through each column
+    For col = 1 To lastColumn
+        'Check if at least one cell in the column is not green
+        For Each cell In rng.Columns(col).Cells
+            If cell.Interior.Color <> RGB(0, 255, 0) Then 'Green color (RGB: 0, 255, 0)
+                'If not green, hide the entire column and exit the loop
+                Columns(col).Hidden = True
+                Exit For
+            End If
+        Next cell
+    Next col
+End Sub
+    */
